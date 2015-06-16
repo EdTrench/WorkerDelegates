c2b00235ad8527ca987fd8dd863e8d3c27f49eae
Edward G Trenchard <ed_trench@yahoo.com>
2015-06-16 11:01:25 +0100

    1

---
+++ WorkerDelegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WorkerDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var worker = new Worker();
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(WorkPerformed);
+            worker.WorkCompleted += WorkCompleted;
+            worker.DoWork(8, WorkType.Cycling);
+            Console.ReadKey();
+        }
+
+        static void WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked: {0} - {1}", e.Hours, e.WorkType);
+        }
+
+        static void WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Work Completed");
+        }
+
+    }
+}
