e038dff61ca66e7df0170827ebe04e0dedb72068
ed_trench@yahoo.com <ed_trench@yahoo.com>
2015-06-17 10:20:45 +0100

    9

---
+++ WorkerDelegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WorkerDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var worker = new Worker();
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(WorkPerformed);
+            worker.WorkCompleted += WorkCompleted;
+            worker.WorkLunch += delegate(int hours)
+            {
+                Console.WriteLine("Hours lunch: {0} - yum, yum", hours);
+            };
+            worker.DoWork(8, WorkType.Cycling);
+            Console.ReadKey();
+        }
+
+        static void WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked: {0} - {1}", e.Hours, e.WorkType);
+        }
+
+        static void WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Work Completed");
+        }
+
+    }
+}
