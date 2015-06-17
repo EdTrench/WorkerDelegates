28526f4d75a16074ce1b6af4d2f0ba13de9b0268
ed_trench@yahoo.com <ed_trench@yahoo.com>
2015-06-17 16:22:20 +0100

    17

---
+++ WorkerDelegates/ProcessData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WorkerDelegates
+{
+    public class ProcessData
+    {
+        public void ProcessDate(int x, int y, BizRulesDelegate del)
+        {
+            var result = del(x, y);
+            Console.WriteLine(result);
+        }
+
+        public void ProcessAction(int x, int y, Action<int, int> action)
+        {
+            action(x, y);
+            Console.WriteLine("Axtion processed!");
+        }
+    }
+}
--- WorkerDelegates/Program.cs
+++ WorkerDelegates/Program.cs
@@ -2,10 +2,21 @@
 
 namespace WorkerDelegates
 {
+    public delegate int BizRulesDelegate(int x, int y);
+
     class Program
     {
         static void Main(string[] args)
         {
+            BizRulesDelegate addDelegate = (x, y) => x + y;
+            BizRulesDelegate multiplyDelegate = (x, y) => x * y;
+
+            var data = new ProcessData();
+            data.ProcessDate(2, 3, multiplyDelegate);
+            
+            Action<int, int> myAddAction = (x, y) => Console.WriteLine(x + y);
+            Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x * y);
+            data.ProcessAction(2, 3, myAddAction);
 
             var worker = new Worker();
             worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(WorkPerformed);
